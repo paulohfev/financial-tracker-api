48c701210e1b4c5d1e82f291cbcd454b23a4fa3c
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-11-20 23:36:07 -0300

    Add validation packages and validation to CreateBillDto

--- src/bills/dto/create-bill.dto.ts
+++ src/bills/dto/create-bill.dto.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateBillDto {
+  @IsNotEmpty()
   title: string;
+
+  @IsNotEmpty()
   description: string;
+
+  @IsNotEmpty()
   value: number;
 }

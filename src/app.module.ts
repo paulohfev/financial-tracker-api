e0baeddd645ec6060ed37896f4010e689b6d5236
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-11-23 10:29:42 -0300

    Add database connection and configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,29 @@
 import { Module } from '@nestjs/common';
 import { BillsModule } from './bills/bills.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [BillsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    BillsModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true,
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })

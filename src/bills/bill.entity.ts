768b78758054090aca30cfa41ffae9b6f1d296d4
Paulo Henrique Evangelista <phfernandesev@gmail.com>
2022-11-24 08:44:27 -0300

    Add Bill entity

---
+++ src/bills/bill.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Bill {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  value: number;
+}
